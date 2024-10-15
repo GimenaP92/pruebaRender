e055f930206f82e77cf2b3141b8039b13eac4dc2
Ignacio Jordan <162840398+Ijordan94@users.noreply.github.com>
2024-10-15 12:46:47 -0300

    deploy2

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# Usa la imagen base de Node.js
+FROM node:18
+
+# Establece el directorio de trabajo
+WORKDIR /usr/src/app
+
+# Copia los archivos de package.json y package-lock.json
+COPY package*.json ./
+
+# Instala las dependencias
+RUN npm install
+
+# Copia el resto de los archivos de la aplicación
+COPY . .
+
+# Compila el proyecto (si es necesario)
+RUN npm run build
+
+# Expone el puerto en el que la aplicación escucha
+EXPOSE 3000
+
+# Comando para iniciar la aplicación
+CMD ["npm", "run", "start"]
--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -1,18 +1,21 @@
+import { User } from 'src/users/entities/user.entity';
 import { DataSource } from 'typeorm';
-import { User } from './entities/user.entity'; // Asegúrate de ajustar las rutas a tus entidades
+import {config as dotenvConfig} from "dotenv"
+
+dotenvConfig({ path: '.env.development' });
 
 const AppDataSource = new DataSource({
-  type: 'postgres',             // Tipo de base de datos
-  host: process.env.DB_HOST,   // Host de la base de datos
-  port: Number(process.env.DB_PORT), // Puerto de la base de datos
-  username: process.env.DB_USER, // Usuario de la base de datos
-  password: process.env.DB_PASSWORD, // Contraseña de la base de datos
-  database: process.env.DB_NAME, // Nombre de la base de datos
-  synchronize: true,            // Habilita la sincronización automática (no recomendado en producción)
-  logging: false,               // Habilita el logging de consultas (útil para desarrollo)
-  entities: [User],             // Aquí puedes agregar tus entidades
-  migrations: [],               // Si tienes migraciones, las puedes incluir aquí
-  subscribers: [],              // Si usas suscriptores, los puedes incluir aquí
+  type: 'postgres',             
+  host: process.env.DB_HOST,   
+  port: Number(process.env.DB_PORT), 
+  username: process.env.DB_USER, 
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_NAME, 
+  synchronize: true,            
+  logging: false,               
+  entities: [User],             
+  
+
 });
 
 export default AppDataSource;
