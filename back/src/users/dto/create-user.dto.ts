30724e3d4cfbb9a0238da14b778aab740da09036
Ignacio Jordan <162840398+Ijordan94@users.noreply.github.com>
2024-10-15 19:26:05 -0300

    con front

--- back/src/users/dto/create-user.dto.ts
+++ back/src/users/dto/create-user.dto.ts
@@ -1,4 +1,3 @@
-import { ApiProperty } from '@nestjs/swagger';
 import {
   IsNotEmpty,
   IsString,
@@ -12,13 +11,8 @@ import {
 
 export class CreateUserDto {
 
-
   @IsNotEmpty()
   @IsEmail()
-  @ApiProperty({
-    description: 'Debe ser un email válido',
-    example: 'juansanchez@ejemplo.com',
-  })
   email: string;
 
   @IsNotEmpty()
@@ -28,11 +22,6 @@ export class CreateUserDto {
     minUppercase: 1,
     minNumbers: 1,
     minSymbols: 1,
-  })
-  @ApiProperty({
-    description:
-      'La contraseña debe contener al menos 1 mayúscula, 1 minúscula, 1 número y 1 caracter especial',
-    example: 'Palabra1*',
   })
   password: string;
 
@@ -44,18 +33,9 @@ export class CreateUserDto {
     minNumbers: 1,
     minSymbols: 1,
   })
-  @ApiProperty({
-    description:
-      'Corrobora que la confirmación sea igual a la contraseña proporcionada',
-    example: 'Palabra1*',
-  })
   confirmPassword: string;
 
   @IsNotEmpty()
-  @ApiProperty({
-    description: 'Agrega tu codigo de área ',
-    example: '543511111111',
-  })
   phone: string;
 
  
