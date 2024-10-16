f5603f35422b76209f73821e0fff8afa37059932
“Gimena” <gimenapascuale@gmail.com>
2024-10-16 13:07:21 -0300

    cors

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,16 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Habilitar CORS para el dominio de tu frontend
   app.enableCors({
-    origin: '*',
+    origin: 'https://pruebarender-4wtf.onrender.com', // Cambia esto al dominio correcto
   });
-
-
 
   await app.listen(3001);
 }
